3f379348a7c6879d0b31e28e0f6e175f7b8227e8
pshemssa <s.patience@alustudent.com>
2023-02-09 06:54:04 -0800

    my script

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+
+
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif num % 5 == 0:
-            print("Buzz ", end="")
-        elif num % 3 == 0:
-            print("Fizz ", end="")
+    for n in range(1, 101):
+        if n % 5 == 0 and n % 3 == 0:
+            print('FizzBuzz', end=' ')
+        elif n % 3 == 0:
+            print('Fizz', end=' ')
+        elif n % 5 == 0:
+            if n == 100:
+                print('Buzz', end=' ')
+            else:
+                print('Buzz', end=' ')
         else:
-            print(f"{num} ", end="")
+            print('{:d}'.format(n), end=' ')
+
+
+if __name__ == '__main__':
+    fizzbuzz()
