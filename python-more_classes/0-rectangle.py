146d602b4a4db73e957cbd5656efcdb6b3c64bc2
pshemssa <s.patience@alustudent.com>
2023-03-17 14:42:53 -0700

    my script

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+"""A module for working with rectangles.
+"""
 
 
 class Rectangle:
-    """Rectangle class."""
+    """A 2D polygon with 4 perpendicular sides.
+    """
     pass
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
