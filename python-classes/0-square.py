2052c4a7d86df66490110fa1569270b927465c01
pshemssa <s.patience@alustudent.com>
2023-03-17 12:11:59 -0700

    my script

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-
-class Square:
-    """An empty class Square."""
-    pass
-
 Square = __import__('0-square').Square
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict_)
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__) 
