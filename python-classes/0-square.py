9f0a4cdf9df143e0d94ee0a3b093aa17228a9294
pshemssa <s.patience@alustudent.com>
2023-03-17 12:22:34 -0700

    my script

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
-Square = __import__('0-square').Square
+#!/usr/bin/python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
+
+class Square:
+    """An empty class Square."""
+    pass
+
+
+if __name__ == '__main__':
 
 my_square = Square()
 print(type(my_square))
-print(my_square.__dict__) 
+print(my_square.__dict__)
