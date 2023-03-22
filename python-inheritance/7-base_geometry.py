e25d7e0c923dbf02eb7c42a0431cf1ceb60c53e6
pshemssa <s.patience@alustudent.com>
2023-03-22 07:25:45 -0700

    my script

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""public instance method"""
+"""
+Contains definition of the class BaseGeometry
+"""
 
 
-class BaseGeometry:
-    """public instance method"""
+class BaseGeometry():
+    """Definition of class BaseGeometry"""
+
     def area(self):
-        """raises an exception"""
+        """Definition of area method.
+           Raises an Exception with message 'area() is not implemented'"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates value"""
-        x = isinstance(value, int)
-        if not x:
+        """Validates value to be an int grater than 0.
+        Args:
+            name (str): name of the value
+            value (unknown): to be validated.
+        Raises:
+            TypeError: if value is not an int.
+            ValueError: if value is <= 0.
+        """
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
