7212a2cab0ca6802642aaae8878ff5d0b2bcfabc
pshemssa <s.patience@alustudent.com>
2023-03-23 16:10:20 -0700

    my script

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""check if object if instance of a class that
-inherited from specified class"""
+
+"""Function that returns True/False."""
 
 
 def inherits_from(obj, a_class):
-    """return True if object is an instance of a class that
-    inherited from specified class"""
-    if issubclass(obj.__class__, a_class) is True:
-        if obj.__class__ is not a_class:
-            return True
-    else:
+    """Returns True/False."""
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
         return False
+    else:
+        return True
