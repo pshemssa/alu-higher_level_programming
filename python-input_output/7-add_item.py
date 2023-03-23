9e103ffef4bf5b9798977d367c76da10d49e6af6
pshemssa <s.patience@alustudent.com>
2023-03-23 15:33:42 -0700

    my script

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""write a script that adds all arguments to a python"""
-import json
+"""This module adds all arguments to a Python list and save them to a file."""
 
-def save_to_json_file(my_obj, filename):
-    """comment function"""
-    with open(filename, 'w') as f:
-        json.dump(my_obj,f)
-def load_from_json_file(filename):
-    """comment function"""
-    with open(filename, 'r') as f:
-        return json.load(f)
-filename = "add_item.json"
-try:
-    items = load_from_json_file(filename)
-except:
-    items = []
-for arg in sys.argv[1:]:
-    items.append(arg)
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
