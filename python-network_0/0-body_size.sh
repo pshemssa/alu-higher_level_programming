b86e73b3ab5a26c53a28c8564dfc196b72f08129
pshemssa <s.patience@alustudent.com>
2023-04-06 11:18:04 +0000

    my script

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL
-curl -sI "$1" | awk '$1 == "Content-Length:" {print $2}'
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
