78370b9d5fe74e9722272faf911846efa23eb308
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-06-11 02:49:09 +0000

    codes

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -7,13 +7,14 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-          s += 'X';
-        }
-        console.log(s);
+        s += 'X';
       }
+      console.log(s);
     }
-  };
+  }
+};
