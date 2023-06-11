81e6eaf9a9e5863e74a739a89ff012a0bea36d17
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-06-11 02:44:27 +0000

    codes

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -7,24 +7,25 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+  
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+      s += 'X';
       }
-        console.log(s);
-      }
+      console.log(s);
     }
-  
-    rotate () {
-      const currentState = this.width;
-      this.width = this.height;
-      this.height = currentState;
-    }
-  
-    double () {
-      this.width = this.width * 2;
-      this.height = this.height * 2;
-    }
-  };
+  }
+
+  rotate () {
+    const currentState = this.width;
+    this.width = this.height;
+    this.height = currentState;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
